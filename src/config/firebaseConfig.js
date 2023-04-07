086dd73b75bf4d86652e9d74d85f7b1460a03e62
talha-intelliscence <talha.rauf@intelliscence.com>
2023-04-07 16:24:47 +0500

    added firebase dependencies

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWFGDxVLEfF0WVJzfLWJ67GBpu_zf_89U",
+  authDomain: "firmground-a0f70.firebaseapp.com",
+  projectId: "firmground-a0f70",
+  storageBucket: "firmground-a0f70.appspot.com",
+  messagingSenderId: "700527284300",
+  appId: "1:700527284300:web:4038e5826981d4e970a4e9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
