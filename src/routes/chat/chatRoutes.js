6e486d305bc2d897d30502b1d6abbbbd3eb90551
talha-intelliscence <talha.rauf@intelliscence.com>
2023-04-07 13:49:48 +0500

    added routes for chat group apis
    routes added for:
    removing member
    adding new admin
    removing an admin
    leaving chat group

--- src/routes/chat/chatRoutes.js
+++ src/routes/chat/chatRoutes.js
@@ -8,6 +8,10 @@ import {
   createChatMessage,
   deleteChatMessage,
   addMembers,
+  removeMemeber,
+  makeAdmin,
+  removeAdmin,
+  leaveChat
 } from '../../controllers/chat/chatController.js';
 import { upload } from '../../config/multerConfig.js';
 
@@ -19,6 +23,10 @@ router.put('/update/:chatId', upload.single('image'), updateChat);
 router.delete('/delete/:chatId', deleteChat);
 
 router.put('/add-members/:chatId', addMembers);
+router.put('/remove-members/:chatId', removeMemeber);
+router.put('/make-admin/:chatId', makeAdmin);
+router.put('/remove-admin/:chatId', removeAdmin);
+router.put('/leave-chat/:chatId', leaveChat);
 
 router.get('/get-messages/:chatId', getChatMessages);
 router.post('/create-message/:chatId', createChatMessage);
