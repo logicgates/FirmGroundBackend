1a61576628af0b98cc3fcb681ca9e2e5a35c414a
Talha-Rauf <128453433+talha-intelliscence@users.noreply.github.com>
2023-04-12 05:48:48 +0500

    added firebase config and dependencies

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,17 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+const admin = require('firebase-admin');
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDWFGDxVLEfF0WVJzfLWJ67GBpu_zf_89U",
-  authDomain: "firmground-a0f70.firebaseapp.com",
-  projectId: "firmground-a0f70",
-  storageBucket: "firmground-a0f70.appspot.com",
-  messagingSenderId: "700527284300",
-  appId: "1:700527284300:web:4038e5826981d4e970a4e9"
-};
+const serviceAccount = require('./serviceAccount.json');
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: `https://${PROJECT_ID}.firebaseio.com`
+});
+
+const db = admin.firestore();
+
+export default db;
