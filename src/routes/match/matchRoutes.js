7ecdbbd15c7cf807fff0c13c591e95f77bfe5ee5
talha-intelliscence <talha.rauf@intelliscence.com>
2023-04-19 09:38:24 +0500

    syntax fix for match controller

--- src/routes/match/matchRoutes.js
+++ src/routes/match/matchRoutes.js
@@ -12,12 +12,12 @@ import { upload } from '../../config/multerConfig.js';
 
 const router = express.Router();
 
-router.get('/active-list/:matchId', getActivePlayers)
+router.get('/group-matches/:chatId', getAllMatches);
+router.get('/active-list/:matchId', getActivePlayers);
 router.post('/create/:userId', createMatch);
 router.put('/update/:matchId', upload.single('image'), updateMatch);
-router.put('/update-status/:matchId', updateParticiationStatus)
-router.put('/add-to-team/:matchId', addPlayerToTeam)
-router.get('/group-matches', getAllMatches)
+router.put('/update-status/:matchId', updateParticiationStatus);
+router.put('/add-to-team/:matchId', addPlayerToTeam);
 router.delete('/delete/:matchId', deleteMatch);
 
 export default router;
