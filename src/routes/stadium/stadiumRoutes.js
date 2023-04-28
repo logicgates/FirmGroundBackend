e4f8861da1be36218f4a8675dacd8eb7cbed6b33
talha-intelliscence <talha.rauf@intelliscence.com>
2023-04-28 16:10:59 +0500

    feat: pitch update route added

--- src/routes/stadium/stadiumRoutes.js
+++ src/routes/stadium/stadiumRoutes.js
@@ -3,7 +3,8 @@ import {
     getAllStadiums,
     addStadium,  
     updateStadium,
-    deleteStadium
+    deleteStadium,
+    updatePitchDetails
 } from '../../controllers/stadium/stadiumController.js';
 import { upload } from '../../config/multerConfig.js';
 
@@ -12,6 +13,7 @@ const router = express.Router();
 router.get('/get-all', getAllStadiums);
 router.post('/add', upload.single('image'), addStadium);
 router.put('/update/:stadiumId', upload.single('image'), updateStadium);
+router.put('/update-pitch/:stadiumId', updatePitchDetails);
 router.delete('/delete/:stadiumId', deleteStadium);
 
 export default router;
