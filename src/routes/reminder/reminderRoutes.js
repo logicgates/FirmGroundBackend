4c65a6d2986b4b6cfd0d07a208a2658d26d08915
talha-intelliscence <talha.rauf@intelliscence.com>
2023-05-22 15:52:13 +0500

    reminder routes added

---
+++ src/routes/reminder/reminderRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { addReminder, deleteReminder, editReminder, viewAllReminders, viewReminder } from '../../controllers/reminder/reminderController';
+
+const router = express.Router();
+
+router.get('/view/:reminderId', viewReminder);
+router.get('/view-all', viewAllReminders);
+router.post('/add', addReminder);
+router.put('/edit/:reminderId', editReminder);
+router.delete('/delete/:reminderId', deleteReminder);
+
+export default router;
