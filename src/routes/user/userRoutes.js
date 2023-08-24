7c936bfe3c49d5842ba5823f1210d4513f472344
talha-intelliscence <128453433+talha-intelliscence@users.noreply.github.com>
2023-08-24 13:23:07 +0500

    user session check function added

--- src/routes/user/userRoutes.js
+++ src/routes/user/userRoutes.js
@@ -19,14 +19,37 @@ import {
   resendRegisterCode,
 } from '../../controllers/auth/authController.js';
 import { upload } from '../../config/multerConfig.js';
+import User from '../../models/user/User.js';
+
+async function checkUserSession (req, res, next) {
+  const userId = req.session.userInfo?.userId;
+  const user = await User.findOne({ _id: userId });
+  if (!user)
+      return res
+          .status(401)
+          .send({ error: 'User timeout. Please login again.' });
+  if (user.deleted?.isDeleted)
+      return res
+          .status(404)
+          .send({ error: 'Not allowed' });
+  
+  const userInfo = {
+      userId: user._id.toString(),
+      email: user.email,
+      profileImage: user.profileImage,
+      deviceId: user.deviceId
+  }
+  req.userInfo = userInfo;
+  next();
+}
 
 const router = express.Router();
 
-router.get('/user/:userId', getUser);
-router.get('/users-list', getUsersList)
-router.post('/change-password/:userId', changePassword);
-router.put('/update/:userId', upload.single('image'), updateUser);
-router.delete('/delete/:userId', deleteUser);
+router.get('/user/:userId', checkUserSession, getUser);
+router.get('/users-list', checkUserSession, getUsersList)
+router.post('/change-password/:userId', checkUserSession, changePassword);
+router.put('/update/:userId', checkUserSession, upload.single('image'), updateUser);
+router.delete('/delete/:userId', checkUserSession, deleteUser);
 
 router.post('/login', login);
 router.post('/register', registerAndSendCode);
